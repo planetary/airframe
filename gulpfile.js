a22c007b2c3077cd488f4806f70e8398d4158277
Matt Strom <matthew.h.strom@gmail.com>
2014-08-11 13:59:59 -0400

    gulp is almost properly integrated - still breaks sometimes

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,79 @@
 var gulp = require('gulp'),
+gulpFilter = require('gulp-filter'),
+mainBowerFiles = require('main-bower-files'),
+rename = require('gulp-rename'),
+uglify = require('gulp-uglify'),
 compass = require('gulp-compass'),
 concat = require('gulp-concat'),
-refresh = require('gulp-livereload'),
-lr = require('tiny-lr'),
-server = lr(),
 minifyCSS = require('gulp-minify-css'),
-embedlr = require('gulp-embedlr'),
 bower = require('gulp-bower');
 
+var destPath = "dist/";
+var srcPath = "src/";
+
+//concat, minify js
 gulp.task('scripts', function() {
-  gulp.src(['src/**/*.js'])
+  gulp.src([srcPath + '**/*.js'])
   .pipe(concat('main.js'))
-  .pipe(gulp.dest('dist/js'))
-  .pipe(refresh(server))
+  .pipe(uglify())
+  .pipe(gulp.dest(destPath + 'js'))
 })
 
+//concat, minify css
 gulp.task('styles', function() {
-  gulp.src(['src/sass/**/*.scss'])
+  gulp.src([srcPath + '/sass/**/*.scss'])
   .pipe(compass({
-    sass: 'src/sass',
-    image: 'src/img',
-    css: 'dist/css',
+    sass: srcPath + 'sass',
+    image: srcPath + 'img',
+    css: destPath + 'css',
     require: ['susy', 'breakpoint']
   }))
   .pipe(minifyCSS())
-  .pipe(gulp.dest('dist/css'))
-  .pipe(refresh(server))
+  .pipe(gulp.dest(destPath + 'css'))
 })
 
+// grab libraries files from bower_components, minify and push in /dist
 gulp.task('bower', function() {
-  return bower()
-    .pipe(gulp.dest('dist/lib/'))
+
+  var jsFilter = gulpFilter('*.js');
+  var cssFilter = gulpFilter('*.css');
+
+  return gulp.src(mainBowerFiles({debugging:true}))
+
+  // grab vendor js files from bower_components, minify and push in /dist
+  .pipe(jsFilter)
+  .pipe(gulp.dest(destPath + 'js/lib'))
+  .pipe(uglify())
+  .pipe(rename({
+        suffix: ".min"
+    }))
+  .pipe(gulp.dest(destPath + 'js/lib'))
+  .pipe(jsFilter.restore())
+
+  // grab vendor css files from bower_components, minify and push in /dist
+  .pipe(cssFilter)
+  .pipe(gulp.dest(destPath + 'css/lib'))
+  .pipe(minifyCSS())
+  .pipe(rename({
+        suffix: ".min"
+    }))
+  .pipe(gulp.dest(destPath + 'css/lib'))
+  .pipe(cssFilter.restore())
+
 });
 
-gulp.task('lr-server', function() {
-  server.listen(35730, function(err) {
-    if(err) return console.log(err);
-  });
-})
-
 gulp.task('html', function() {
-  gulp.src("src/*.html")
-  .pipe(embedlr())
-  .pipe(gulp.dest('dist/'))
-  .pipe(refresh(server));
+  gulp.src(srcPath + '*.html')
+  .pipe(gulp.dest(destPath))
 })
 
 gulp.task('watch', function() {
-  gulp.watch('src/js/**', ['scripts']);
-  gulp.watch('src/sass/**', ['styles']);
-  gulp.watch('src/**/*.html', ['html']);
+  gulp.watch(srcPath + 'js/**', ['scripts']);
+  gulp.watch(srcPath + 'sass/**', ['styles']);
+  gulp.watch(srcPath + '**/*.html', ['html']);
 })
 
 gulp.task('default', ['bower', 'scripts', 'styles', 'html'])
 
-gulp.task('watch', ['lr-server', 'watch'])
+gulp.task('watch', ['watch'])
 
--- readme.md
+++ readme.md
@@ -2,9 +2,14 @@
 ## A Project Starter Kit, made with love by [Planetary](http://planetary.io)
 
 **Dependencies:**
+- [NodeJS](http://nodejs.org/)
 - [Compass](http://compass-style.org/)
 - [Susy](http://susy.oddbird.net/)
 - [Breakpoint](http://breakpoint-sass.com/)
 
 **Getting started:**
-- Run 'compass watch' to begin compiling sass as you develop
+1. Run `npm install` to install all nodeJS packages
+2. Run `gulp` to build the project.
+
+**Developing**
+1. Run `gulp watch` to continuously build the project
