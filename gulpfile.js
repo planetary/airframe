059b774e9fe274380252952e7ed744403de85a80
Matt Strom <matthew.h.strom@gmail.com>
2014-10-06 12:18:23 -0400

    converted jade and gulpfile to 4 spaces

--- gulpfile.js
+++ gulpfile.js
@@ -17,89 +17,89 @@ var srcPath = "src/";
 
 //this will handle errors for us
 function handleError(err) {
-  console.log(err.toString());
-  this.emit('end');
+    console.log(err.toString());
+    this.emit('end');
 }
 
 //concat, minify js
 gulp.task('scripts', function() {
-  gulp.src([srcPath + '**/*.js'])
-  .pipe(concat('main.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest(destPath + 'js'))
+    gulp.src([srcPath + '**/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(destPath + 'js'))
 })
 
 //concat, minify css
 gulp.task('styles', function() {
-  gulp.src([srcPath + '/sass/**/*.scss'])
-  .pipe(compass({
-    sass: srcPath + 'sass',
-    image: srcPath + 'img',
-    css: destPath + 'css',
-    require: ['susy', 'breakpoint', 'modular-scale']
-  }))
-  .on('error', handleError)
-  .pipe(minifyCSS())
-  .pipe(gulp.dest(destPath + 'css'))
+    gulp.src([srcPath + '/sass/**/*.scss'])
+    .pipe(compass({
+        sass: srcPath + 'sass',
+        image: srcPath + 'img',
+        css: destPath + 'css',
+        require: ['susy', 'breakpoint', 'modular-scale']
+    }))
+    .on('error', handleError)
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(destPath + 'css'))
 })
 
 // grab libraries files from bower_components, minify and push in /dist
 gulp.task('bower', function() {
 
-  var jsFilter = gulpFilter('*.js');
-  var cssFilter = gulpFilter('*.css');
+    var jsFilter = gulpFilter('*.js');
+    var cssFilter = gulpFilter('*.css');
 
-  var check = fs.existsSync('./bower_components');
-  if (!check) {
-    console.log('No bower components installed.');
-    return;
-  }
+    var check = fs.existsSync('./bower_components');
+    if (!check) {
+        console.log('No bower components installed.');
+        return;
+    }
 
-  return gulp.src(mainBowerFiles({debugging:true}))
+    return gulp.src(mainBowerFiles({debugging:true}))
 
-  // grab vendor js files from bower_components, minify and push in /dist
-  .pipe(jsFilter)
-  .pipe(gulp.dest(destPath + 'js/lib'))
-  .pipe(uglify())
-  .pipe(rename({
+    // grab vendor js files from bower_components, minify and push in /dist
+    .pipe(jsFilter)
+    .pipe(gulp.dest(destPath + 'js/lib'))
+    .pipe(uglify())
+    .pipe(rename({
         suffix: ".min"
     }))
-  .pipe(gulp.dest(destPath + 'js/lib'))
-  .pipe(jsFilter.restore())
+    .pipe(gulp.dest(destPath + 'js/lib'))
+    .pipe(jsFilter.restore())
 
-  // grab vendor css files from bower_components, minify and push in /dist
-  .pipe(cssFilter)
-  .pipe(gulp.dest(destPath + 'css/lib'))
-  .pipe(minifyCSS())
-  .pipe(rename({
+    // grab vendor css files from bower_components, minify and push in /dist
+    .pipe(cssFilter)
+    .pipe(gulp.dest(destPath + 'css/lib'))
+    .pipe(minifyCSS())
+    .pipe(rename({
         suffix: ".min"
     }))
-  .pipe(gulp.dest(destPath + 'css/lib'))
-  .pipe(cssFilter.restore())
+    .pipe(gulp.dest(destPath + 'css/lib'))
+    .pipe(cssFilter.restore())
 
 });
 
 //compile jade files
 gulp.task('templates', function() {
-  gulp.src(srcPath + 'jade/views/**/*.jade')
+    gulp.src(srcPath + 'jade/views/**/*.jade')
     .pipe(jade({
-      'basedir': srcPath + 'jade'
+        'basedir': srcPath + 'jade'
     }))
     .pipe(gulp.dest(destPath))
 });
 
 // linting tasks to check for manifest adherance
 gulp.task('scss-lint', function() {
-  gulp.src(srcPath + '/sass/**/*.scss')
+    gulp.src(srcPath + '/sass/**/*.scss')
     .pipe(scsslint({
         'config': 'lint.yml',
     }));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(srcPath + 'js/**', ['scripts']);
-  gulp.watch(srcPath + 'sass/**', ['scss-lint', 'styles']);
-  gulp.watch(srcPath + '**/*.jade', ['templates']);
+    gulp.watch(srcPath + 'js/**', ['scripts']);
+    gulp.watch(srcPath + 'sass/**', ['scss-lint', 'styles']);
+    gulp.watch(srcPath + '**/*.jade', ['templates']);
 })
 
 gulp.task('default', ['bower', 'scripts', 'styles', 'templates'])
