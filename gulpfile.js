df9354cfa7906667e484281fd80a569b07a4d059
Joshua Gross <joshua@gross.is>
2015-03-05 14:12:28 -0500

    Remove bower; add Browserify + Watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulp = require( 'gulp' ),
     gulpFilter = require( 'gulp-filter' ),
-    mainBowerFiles = require( 'main-bower-files' ),
     rename = require( 'gulp-rename' ),
     uglify = require( 'gulp-uglify' ),
     sass = require( 'gulp-sass' ),
     concat = require( 'gulp-concat' ),
     csso = require( 'gulp-csso' ),
-    bower = require( 'gulp-bower' ),
     fs = require( 'fs' ),
     jade = require( 'gulp-jade' ),
     scsslint = require( 'gulp-scss-lint' ),
@@ -14,77 +12,66 @@ var gulp = require( 'gulp' ),
     image = require( 'gulp-image' ),
     prefix = require( 'gulp-autoprefixer' ),
     gutil = require( 'gulp-util' ),
+    sourcemaps = require( 'gulp-sourcemaps' ),
     colors = gutil.colors,
     notify = require( 'gulp-notify' ),
     path = require( 'path' ),
-    install = require( 'gulp-install' );
+    watchify = require( 'watchify' ),
+    browserify = require( 'browserify' ),
+    source = require( 'vinyl-source-stream' ),
+    buffer = require( 'vinyl-buffer' );
 
 var destPath = 'dist/';
 var srcPath = 'src/';
 
 // this will handle errors for us
-function handleError( err ) {
-    console.log( err.toString() );
+var errorHandler = function( err ) {
+    notify.onError( {
+        message: "<%= error.message %>"
+    } ).apply( this, arguments );
+
     this.emit( 'end' );
-}
+};
 
 // concat, minify js
-gulp.task( 'scripts', function() {
-    gulp.src( [ srcPath + '**/*.js' ] )
-        .pipe( concat( 'main.js' ) )
-        .pipe( uglify() )
-        .pipe( gulp.dest( destPath + 'js' ) );
+var args = watchify.args;
+args.debug = true;
+
+var bundler = browserify( './src/js/index.js', args );
+// add any other browserify options or transforms here
+bundler.transform( 'brfs' );
+
+gulp.task( 'bundle:dev', bundle ); // so you can run `gulp bundle:dev` to build the file
+gulp.task( 'bundle:watch', function() {
+    var watcher = watchify( bundler );
+    bundler.on( 'update', watcher ); // on any dep update, runs the bundler
 } );
+
+function bundle() {
+  return bundler.bundle()
+    // log errors if they happen
+    .on( 'error', errorHandler )
+    .pipe( source( 'bundle.js' ) )
+    // optional, remove if you dont want sourcemaps
+      .pipe( buffer() )
+      .pipe( sourcemaps.init( { loadMaps: true } ) ) // loads map from browserify file
+      .pipe( sourcemaps.write( './' ) ) // writes .map file
+    //
+    .pipe( gulp.dest( './dist/js' ) )
+    .pipe( notify ( { message: 'browserify bundler task complete' } ) );
+}
 
 // concat, minify css
 gulp.task( 'styles', function() {
     gulp.src( [ srcPath + '/sass/**/*.scss' ] )
         .pipe( sass( {
             sourcemap: true,
-            sourcemapPath: srcPath + '/sass',
-            loadPath: [ 'bower_components/' ]
+            sourcemapPath: srcPath + '/sass'
         } ) )
-        .on( 'error', handleError )
+        .on( 'error', errorHandler )
         .pipe( csso() )
         .pipe( prefix() )
         .pipe( gulp.dest( destPath + 'css' ) );
-} );
-
-gulp.task( 'install-dep', function() {
-    gulp.src( [ './bower.json', './package.json' ] )
-        .pipe( install() );
-} );
-
-// grab libraries files from bower_components, minify and push in /dist
-gulp.task( 'bower', function() {
-    var jsFilter = gulpFilter( '*.js' ),
-        cssFilter = gulpFilter( '*.css' );
-
-    var check = fs.existsSync( './bower_components' );
-    if ( !check ) {
-        console.log( 'No bower components installed.' );
-        return;
-    }
-
-    return gulp.src( mainBowerFiles( { debugging: true } ) )
-        // grab vendor js files from bower_components, minify and push in /dist
-        .pipe( jsFilter )
-        .pipe( gulp.dest( destPath + 'js/lib' ) )
-        .pipe( uglify() )
-        .pipe( rename( {
-            suffix: ".min"
-        } ) )
-        .pipe( gulp.dest( destPath + 'js/lib' ) )
-        .pipe( jsFilter.restore() )
-        // grab vendor css files from bower_components, minify and push in /dist
-        .pipe( cssFilter )
-        .pipe( gulp.dest( destPath + 'css/lib' ) )
-        .pipe( minifyCSS() )
-        .pipe( rename( {
-            suffix: ".min"
-        } ) )
-        .pipe( gulp.dest( destPath + 'css/lib' ) )
-        .pipe( cssFilter.restore() );
 } );
 
 //compile jade files
@@ -93,20 +80,20 @@ gulp.task( 'templates', function() {
         .pipe( jade( {
             'basedir': srcPath + 'jade'
         } ) )
+        .on( 'error', errorHandler )
         .pipe( gulp.dest( destPath ) );
 } );
 
 //optimize images
 gulp.task( 'img', function () {
-  return gulp.src( [ srcPath + 'img/**' ] )
-      .pipe( newer( destPath + 'img' ) )
-      .pipe( image() )
-      .pipe( gulp.dest( destPath + 'img' ) );
+    return gulp.src( [ srcPath + 'img/**' ] )
+        .pipe( newer( destPath + 'img' ) )
+        .pipe( image() )
+        .pipe( gulp.dest( destPath + 'img' ) );
 } );
 
 // linting tasks to check for manifest adherance
-
-var myCustomReporter = function ( file ) {
+var scssLinter = function( file ) {
     if ( !file.scsslint.success ) {
         gutil.log( colors.red( file.scsslint.issues.length + ' SCSS issue' + ( file.scsslint.issues.length == 1 ? '' : 's' ) + ' found in ' ) + file.path );
         var messages = new Array();
@@ -120,21 +107,21 @@ var myCustomReporter = function ( file ) {
             "icon": path.join( __dirname, "fail.png" ), // case sensitive
         } ) );
     }
-}
+};
 
 gulp.task( 'scss-lint', function() {
     gulp.src( srcPath + '/sass/**/*.scss' )
         .pipe( scsslint( {
             'config': '.scss-lint.yml',
-            'customReport': myCustomReporter
+            'customReport': scssLinter
         } ) );
 } );
 
 gulp.task( 'watch', function() {
-    gulp.watch( srcPath + 'js/**', [ 'scripts' ] );
+    gulp.watch( srcPath + 'js/**', [ 'bundle:watch' ] );
     gulp.watch( srcPath + 'sass/**', [ 'scss-lint', 'styles' ] );
     gulp.watch( srcPath + '**/*.jade', [ 'templates' ] );
     gulp.watch( srcPath + 'img/**', [ 'img' ] );
-} )
+} );
 
-gulp.task( 'default', [ 'install-dep', 'scripts', 'styles', 'templates', 'img' ] );
+gulp.task( 'default', [ 'bundle:dev', 'styles', 'templates', 'img' ] );
