0d2d7a4caf36eca48305c6af102e05a2e7fe0c81
Radu Dan <dan@planetary.io>
2015-06-02 19:21:20 +0300

    Combine js linting tasks (jscs and jshint) into a single report

--- tasks/js.js
+++ tasks/js.js
@@ -1,5 +1,4 @@
 module.exports = function( gulp, plugins ) {
-    var watching = false;
     var paths = [
         // JS files to watch and lint
         'tasks/*.js',
@@ -8,32 +7,19 @@ module.exports = function( gulp, plugins ) {
     ];
 
 
-    gulp.task( 'lint:js:jscs', 'lints all javascript files with jscs', function() {
-        return gulp
-            .src( paths )
-            .pipe( plugins.jscs() )
-            .pipe( plugins.jscsStylish() );
-    } );
-
-
-    gulp.task( 'lint:js:jshint', 'lints all javascript files with jshint', function() {
+    gulp.task( 'lint:js', 'lints all javascript files with both jscs and jshint', function() {
         return gulp
             .src( paths )
             .pipe( plugins.jshint() )
+            .pipe( plugins.jscs() )
+            .on( 'error', function() { } )
+            .pipe( plugins.jscsStylish.combineWithHintResults() )
             .pipe( plugins.jshint.reporter( 'jshint-stylish' ) )
-            .pipe( plugins.if( !watching, plugins.jshint.reporter( 'fail' ) ) );
+            .pipe( plugins.jshint.reporter( 'fail' ) );
     } );
 
 
-    gulp.task( 'lint:js', 'lints all javascript files with both jscs and jshint', [
-        'lint:js:jshint',
-        'lint:js:jscs'
-    ] );
-
-
     gulp.task( 'watch:js', 'waits for javascript files to change, then lints them ', function() {
-        watching = true;
-
         return gulp.watch( paths, [ 'lint:js' ] );
     } );
 };
