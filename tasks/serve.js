429a0b7d053e82c7a4f66301b57c8e2e97950f21
Radu Dan <dan@planetary.io>
2015-06-08 17:16:28 +0300

    Rework browser sync

--- tasks/serve.js
+++ tasks/serve.js
@@ -1,15 +1,34 @@
-var extend = require( 'util' )._extend,
-    http = require( 'http' ),
-    ecstatic = require( 'ecstatic' );
+var browserSync = require( 'browser-sync' ),
+    ecstatic = require( 'ecstatic' ),
+    http = require( 'http' );
 
-module.exports = function( gulp, plugins, path ) {
-    gulp.task( 'serve', 'serves the build folder on localhost port 4900', function() {
-        var port = 4900;
+
+module.exports = function( gulp ) {
+    var paths = [
+        // files that should be watched for changes and pushed onto browsers when they do
+        './build/**/*'
+    ];
+    var ports = {
+        'frontend': 3000,
+        'backend': 4900
+    };
+
+
+    gulp.task( 'serve:browsersync', 'proxies the localhost server via BrowserSync to ' +
+                                    'dynamically update assets', function() {
+        browserSync( {
+            'port': ports.frontend,
+            'files': paths,
+            'proxy': 'http://localhost:' + ports.backend
+        } );
+    } );
+
+
+    gulp.task( 'serve', 'serves static templates locally', [ 'serve:browsersync' ], function() {
+        // quick hack; should be replaced with nodemon or something similar as a backend is decided
         http.createServer( ecstatic( {
-            root: 'build',
-            cache: 0
-        } ) ).listen( port );
-
-        plugins.util.log( plugins.util.colors.blue( 'Server started, listening on ' + port ) );
+            'root': './build',
+            'cache': 0
+        } ) ).listen( ports.backend );
     } );
 };
