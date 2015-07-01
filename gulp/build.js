924b31d010791b85ec0ed1bcd6ff894f9447f239
Radu Dan <dan@planetary.io>
2015-07-01 20:39:34 +0300

    Switch to gulp-rev-all in non-local environments

--- gulp/build.js
+++ gulp/build.js
@@ -1,4 +1,7 @@
-module.exports = function(gulp) {
+var path = require('path');
+
+
+module.exports = function(gulp, plugins) {
     gulp.task('build', 'builds all the registered static resources from assets into build', [
         // Add your build tasks here (must be prefixed with build:)
         'build:fonts',
@@ -6,5 +9,20 @@ module.exports = function(gulp) {
         'build:scripts',
         'build:styles',
         'build:templates'
-    ]);
+    ], function(next) {
+        if(['development', '', undefined].indexOf(process.env.NODE_ENV) !== -1) {
+            // don't create revisions during development
+            return next();
+        }
+
+        var rev = new plugins.revAll();
+
+        gulp.src(path.join(gulp.outputPath, '**', '*'))
+            .pipe(rev.revision())
+            .pipe(gulp.dest(gulp.outputPath))
+            .pipe(rev.manifestFile())
+            .pipe(gulp.dest(gulp.outputPath))
+            .on('finish', next)
+            .on('error', next);
+    });
 };
