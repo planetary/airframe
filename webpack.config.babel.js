ad55a1de6614e7efaa589afa166e91450b2f5d82
Andrew Coelho <acoelho.baa@gmail.com>
2017-04-04 13:39:54 -0400

    Update to webpack 2 (#46)
    * Update to webpack 2
    
    * Update node version in circle.yml
    
    * Remove unnecessary cross-env
    
    * devDependencies -> dependencies
    
    * Update to use planetary's public stylelint config
    
    * Update stylelint dependencies

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -11,10 +11,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const ReloadPlugin = require('reload-html-webpack-plugin');
 const WebpackMD5Hash = require('webpack-md5-hash');
 
-const webpackValidator = require('webpack-validator');
 const {getIfUtils, removeEmpty, propIf} = require('webpack-config-utils');
-const {ifProd, ifNotProd} = getIfUtils(process.env.NODE_ENV);
-const isBuild = process.env.MODE === 'build';
 
 // Loads an HtmlWebpackPlugin for each template in the assets/views directory
 const loadHtmlPlugin = (src, base = '') => {
@@ -34,134 +31,140 @@ const loadHtmlPlugin = (src, base = '') => {
     });
 };
 
-module.exports = webpackValidator({
-    entry: [
-        './assets/scripts/index.js'
-    ],
-    output: {
-        filename: ifProd('scripts/bundle-[chunkhash:8].js', 'scripts/bundle.js'),
-        path: path.resolve('build'),
-        pathinfo: ifNotProd(),
-        publicPath: process.env.PUBLIC_PATH || '/'
-    },
-    devtool: ifProd('source-map', 'eval'),
-    devServer: {
-        stats: 'errors-only',
-        historyApiFallback: ifNotProd()
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/,
-                query: {
-                    cacheDirectory: ifNotProd()
+module.exports = (env) => {
+    const {ifProd, ifNotProd} = getIfUtils(env);
+    const isBuild = Boolean(env.build);
+    const withDocs = Boolean(env.docs);
+
+    return {
+        entry: [
+            './assets/scripts/index.js'
+        ],
+        output: {
+            filename: ifProd('scripts/bundle-[chunkhash:8].js', 'scripts/bundle.js'),
+            path: path.resolve('build'),
+            pathinfo: ifNotProd(),
+            publicPath: process.env.PUBLIC_PATH || '/'
+        },
+        devtool: ifProd('source-map', 'eval'),
+        devServer: {
+            stats: 'errors-only',
+            historyApiFallback: ifNotProd()
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    loader: ifProd(
+                        ExtractTextPlugin.extract({
+                            fallbackLoader: 'style-loader',
+                            loader: 'css-loader!postcss-loader'
+                        }), 'style-loader!css-loader!postcss-loader')
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    query: {
+                        cacheDirectory: ifNotProd()
+                    }
+                },
+                {
+                    test: /\.pug$/,
+                    loader: 'pug-loader'
+                },
+                {
+                    test: /\.(jpe?g|png|gif)$/,
+                    loaders: [
+                        'url-loader?limit=10000&name=images/[name].[ext]',
+                        'image-webpack-loader?bypassOnDebug&optimizationLevel=2&interlaced=false'
+                    ]
+                },
+                {
+                    test: /\.svg$/,
+                    loaders: [
+                        'file-loader?name=images/[name].[ext]',
+                        'image-webpack-loader?bypassOnDebug&optimizationLevel=2&interlaced=false'
+                    ]
+                },
+                {
+                    test: /\.woff$/,
+                    loader: 'url-loader',
+                    query: {
+                        name: 'fonts/[name].[ext]',
+                        limit: 5000,
+                        mimetype: 'application/font-woff'
+                    }
+                },
+                {
+                    test: /\.ttf$|\.eot$/,
+                    loader: 'file-loader',
+                    query: {
+                        name: 'fonts/[name].[ext]'
+                    }
                 }
-            },
-            {
-                test: /\.css$/,
-                loader: ifProd(
-                    ExtractTextPlugin.extract('style', 'css!postcss'),
-                    'style!css!postcss'
-                )
-            },
-            {
-                test: /\.pug$/,
-                loader: 'pug'
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/,
-                loaders: [
-                    'url?limit=10000&name=images/[name].[ext]',
-                    'image-webpack?bypassOnDebug&optimizationLevel=2&interlaced=false'
-                ]
-            },
-            {
-                test: /\.svg$/,
-                loaders: [
-                    'file?name=images/[name].[ext]',
-                    'image-webpack?bypassOnDebug&optimizationLevel=2&interlaced=false'
-                ]
-            },
-            {
-                test: /\.woff$/,
-                loader: 'url',
-                query: {
-                    name: 'fonts/[name].[ext]',
-                    limit: 5000,
-                    mimetype: 'application/font-woff'
-                }
-            },
-            {
-                test: /\.ttf$|\.eot$/,
-                loader: 'file',
-                query: {
-                    name: 'fonts/[name].[ext]'
-                }
-            }
-        ]
-    },
-    postcss(wp) {
-        return [
-            require('postcss-import')({
-                addDependencyTo: wp
-            }),
-            require('postcss-mixins'),
-            require('postcss-cssnext')
-        ];
-    },
-    plugins: removeEmpty([
-        ifNotProd(new ProgressBarPlugin()),
-        isBuild ? undefined : ifNotProd(new ReloadPlugin()), // eslint-disable-line
-        ifProd(new ExtractTextPlugin('styles/styles-[chunkhash:8].css')),
-        ifProd(new PurifyPlugin({
-            basePath: __dirname,
-            paths: propIf(process.env.WITH_DOCS || false,
-                [
-                    'assets/templates/**/*.pug',
-                    'docs/**/*.pug'
-                ],
-                [
-                    'assets/templates/**/*.pug'
-                ]
-            ),
-            purifyOptions: {
-                minify: true
-            }
-        })),
-        new StyleLintPlugin({
-            configFile: '.stylelintrc',
-            files: 'assets/styles/**/*.css'
-        }),
-        ifProd(new InlineManifestPlugin()),
-        ifProd(new webpack.optimize.CommonsChunkPlugin({
-            name: 'manifest'
-        })),
-        ifProd(new webpack.optimize.DedupePlugin()),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new WebpackMD5Hash(),
-        ...loadHtmlPlugin(propIf(
-            process.env.WITH_DOCS || false,
-            './docs',
-            './assets/templates/views'
-        )),
-        ...ifNotProd(loadHtmlPlugin('./docs', 'docs/'), []),
-        new CopyPlugin(removeEmpty([
-            // Copy fonts to build directory
-            {from: 'fonts', to: 'fonts'},
-            {from: 'favicons', to: '../build'},
-            ifNotProd({from: 'images', to: 'images'})
-        ]), {
-            ignore: [
-                '.*'
             ]
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: ifProd('"prod"', '"dev"')
-            }
-        }),
-        new webpack.NamedModulesPlugin()
-    ])
-});
+        },
+        plugins: removeEmpty([
+            ifNotProd(new ProgressBarPlugin()),
+            isBuild ? undefined : ifNotProd(new ReloadPlugin()), // eslint-disable-line
+            new webpack.LoaderOptionsPlugin({
+                options: {
+                    postcss: [
+                        require('postcss-import'),
+                        require('postcss-mixins'),
+                        require('postcss-cssnext')
+                    ]
+                }
+            }),
+            ifProd(new ExtractTextPlugin('styles/styles-[chunkhash:8].css')),
+            ifProd(new PurifyPlugin({
+                basePath: __dirname,
+                paths: propIf(env.docs || false,
+                    [
+                        'assets/templates/**/*.pug',
+                        'docs/**/*.pug'
+                    ],
+                    [
+                        'assets/templates/**/*.pug'
+                    ]
+                ),
+                purifyOptions: {
+                    minify: true
+                }
+            })),
+            new StyleLintPlugin({
+                configFile: '.stylelintrc',
+                files: 'assets/styles/**/*.css'
+            }),
+            ifProd(new InlineManifestPlugin()),
+            ifProd(new webpack.optimize.CommonsChunkPlugin({
+                name: 'manifest'
+            })),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new WebpackMD5Hash(),
+            ...loadHtmlPlugin(propIf(
+                withDocs || false,
+                './docs',
+                './assets/templates/views'
+            )),
+            ...ifNotProd(loadHtmlPlugin('./docs', 'docs/'), []),
+            new CopyPlugin(removeEmpty([
+                // Copy fonts to build directory
+                {from: 'fonts', to: 'fonts'},
+                {from: 'favicons', to: '../build'},
+                ifNotProd({from: 'images', to: 'images'})
+            ]), {
+                ignore: [
+                    '.*'
+                ]
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: ifProd('"prod"', '"dev"')
+                }
+            }),
+            new webpack.NamedModulesPlugin()
+        ])
+    };
+};
